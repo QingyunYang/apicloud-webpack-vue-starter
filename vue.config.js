0cf8fa35b14ade910a5506f722ba4c043b9fc9a8
yangqingyun <yangqingyun@hotmail.com>
2020-03-16 17:36:38 +0800

    添加屏幕适配配置

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,63 @@
 const path = require('path')
 const glob = require('glob')
+const autoprefixer = require('autoprefixer');
+const pxtoviewport = require('postcss-px-to-viewport');
+const pxtorem = require('postcss-pxtorem');
 
+// 获取pages配置
+const getPagesConfig = () => {
+  const index = './src/pages/app.js';
+  const pages = { index }
 
-const pages = {
-  index: './src/pages/app.js'
+  const pattern = './src/pages/**/app.js'
+  const paths = glob.sync(pattern, { ignore: index })
+  paths.forEach(item => {
+    const chunk = item.split('./src/pages/')[1].split('/app.js')[0]
+    pages[chunk] = {
+      entry: item,
+      title: chunk,
+      chunks: ['chunk-vendors', 'chunk-common', chunk]
+    }
+  })
+  return pages
 }
-const pathList = glob.sync('./src/pages/**/app.js', {
-  ignore: './src/pages/app.js'
-})
-pathList.forEach(item => {
-  const chunk = item.split('./src/pages/')[1].split('/app.js')[0]
-  pages[chunk] = {
-    entry: item,
-    title: chunk,
-    chunks: ['chunk-vendors', 'chunk-common', chunk]
+
+/**
+ * 获取postcss插件配置
+ * @param {string} scheme 可选，屏幕适配方案，可选 vw，rem
+ */
+const getPostcssPlugins = scheme => {
+  const plugins = [
+    autoprefixer()
+  ]
+
+  if (scheme === 'vw') {
+    plugins.push(
+      pxtoviewport({ viewportWidth: 375 })
+    )
+  } else if (scheme === 'rem') {
+    plugins.push(
+      pxtorem({
+        rootValue: 75,
+        propList: ['*']
+      })
+    )
   }
-})
+
+  return plugins
+}
 
 module.exports = {
-  pages,
+  pages: getPagesConfig(),
   assetsDir: 'static',
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: getPostcssPlugins('rem')
+      }
+    }
+  },
   configureWebpack: {
     resolve: {
       alias: {
