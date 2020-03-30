39136551a46796e2fdab3d39cc372c91a7d75d52
yangqingyun <yangqingyun@hotmail.com>
2020-03-30 14:35:49 +0800

    add closeWin method

---
+++ src/utils/api/closeWin.js
@@ -0,0 +1,5 @@
+const closeWin = () => {
+  window.history.back()
+}
+
+export default closeWin
--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,8 +1,10 @@
 import openWin from './openWin'
+import closeWin from './closeWin'
 import ajax from './ajax'
 
 const api = {
   openWin,
+  closeWin,
   ajax
 }
 
