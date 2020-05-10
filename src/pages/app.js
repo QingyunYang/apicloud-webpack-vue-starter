540ab70e7236693fadc0ff1e6e92eb6af0def10e
yangqingyun <yangqingyun@hotmail.com>
2020-05-10 12:41:01 +0800

    添加移动端调试功能

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,54 +1,17 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import {
-  isH5,
-  isLoader,
-  isNative,
-  isChrome
-} from '@/utils'
-import store from '@/store'
+// import Vue from 'vue'
+// import eruda from 'eruda'
+// import VueRouter from 'vue-router'
+// import store from '@/store'
+// import { isH5, isLoader, isNative, isChrome } from '@/utils'
+import { mount } from '@/utils'
 import App from './app.vue'
 import Home from './home.vue'
 import Mine from './mine'
-import '@/assets/less/global.less'
 
-Vue.config.productionTip = false
-
-console.log(`是否为开发环境: ${process.env.NODE_ENV === 'development'}`)
-console.log(`是否为chrome开发环境: ${isChrome}`)
-console.log(`是否为loader开发环境: ${isLoader}`)
-
-
-console.log(`是否为生产环境: ${process.env.NODE_ENV === 'production'}`)
-console.log(`是否为h5生产环境: ${isH5}`)
-console.log(`是否为native生产环境: ${isNative}`)
-
-Vue.use(VueRouter)
 const routes = [
   { path: '/', redirect: '/home' },
   { path: '/home', name: 'home', component: Home },
   { path: '/mine', name: 'mine', component: Mine },
 ]
-const router = new VueRouter({ routes })
 
-const render = () => {
-  new Vue({
-    store,
-    router,
-    render: h => h(App),
-  }).$mount('#app')
-}
-
-if (isLoader || isNative) {
-  window.apiready = () => {
-    Vue.prototype.api = window.api
-    render()
-  }
-}
-
-if (isChrome || isH5) {
-  import('@/utils/api').then(module => {
-    Vue.prototype.api = module.default
-    render()
-  })
-}
+mount(App, routes);
--- src/utils/mount.js
+++ src/utils/mount.js
@@ -1,44 +1,61 @@
 import Vue from 'vue'
-import { 
-  isH5,
-  isLoader,
-  isNative,
-  isChrome
-} from '@/utils'
+import eruda from 'eruda'
+import VueRouter from 'vue-router'
 import store from '@/store'
+import { isH5, isLoader, isNative, isChrome } from '@/utils'
 import '@/assets/less/global.less'
+
+// 开启移动端调试
+const { VUE_APP_CONSOLE } = process.env
+if (VUE_APP_CONSOLE === 'true') {
+  eruda.init()
+}
 
 Vue.config.productionTip = false
 
-const mount = component => {
+/**
+ * 根据路由信息，生成路由
+ * @param {array} routes 
+ */
+const initRouter = routes => {
+  if (routes.length) {
+    Vue.use(VueRouter)
+    return new VueRouter({ routes })
+  }
+  return null
+}
 
-  console.log(`是否为开发环境: ${process.env.NODE_ENV === 'development'}`)
+const mount = (component, routes = []) => {
+
+  const { NODE_ENV } = process.env
+  console.log(`是否为开发环境: ${NODE_ENV === 'development'}`)
   console.log(`是否为chrome开发环境: ${isChrome}`)
   console.log(`是否为loader开发环境: ${isLoader}`)
 
-
-  console.log(`是否为生产环境: ${process.env.NODE_ENV === 'production'}`)
+  console.log(`是否为生产环境: ${NODE_ENV === 'production'}`)
   console.log(`是否为h5生产环境: ${isH5}`)
   console.log(`是否为native生产环境: ${isNative}`)
 
-  const render = () => {
-    new Vue({
-      store,
-      render: h => h(component),
-    }).$mount('#app')
+  /**
+   * 实例化Vue
+   */
+  const createInstance = () => {
+    const router = initRouter(routes)
+    const render = h => h(component)
+    new Vue({ store, router, render }).$mount('#app')
   }
   
   if (isLoader || isNative) {
     window.apiready = () => {
       Vue.prototype.api = window.api
-      render()
+      createInstance()
     }
   }
   
   if (isChrome || isH5) {
     import('@/utils/api').then(module => {
       Vue.prototype.api = module.default
-      render()
+      createInstance()
     })
   }
 }
